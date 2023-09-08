9b120d5d5436fef14042ab66a902502e3b693bfb
David Cary <shimeran@tutanota.com>
2023-09-08 08:15:55 -0400

    including new test file

---
+++ tests/resolver.test.ts
@@ -0,0 +1,69 @@
+import {
+  TypeConversionResolver,
+  JSTypeName,
+  DEFAULT_TYPE_CONVERTORS
+} from "../src/index"
+
+const resolver = new TypeConversionResolver(DEFAULT_TYPE_CONVERTORS)
+
+describe("TypeConversionResolver", () => {
+  describe("getRequestSchema", () => {
+    test("should convert type name to an schema", () => {
+      const schema = resolver.getRequestSchema(JSTypeName.STRING)
+      expect(schema).toEqual({ type: 'string' })
+    })
+    test("should return first matching schema of union", () => {
+      const schema = resolver.getRequestSchema(
+        {
+          anyOf: [
+            JSTypeName.NUMBER,
+            JSTypeName.STRING
+          ]
+        },
+        "hi"
+      )
+      expect(schema).toEqual({ type: 'string' })
+    })
+    test("should resolve references from context", () => {
+      const schema = resolver.getRequestSchema(
+        {
+          $ref: 'name'
+        },
+        null,
+        {
+          schemas: {
+            'name': {
+              type: JSTypeName.STRING
+            }
+          }
+        }
+      )
+      expect(schema).toEqual({ type: 'string' })
+    })
+    test("should use parent context to resolves references", () => {
+      const schema = resolver.getRequestSchema(
+        {
+          $ref: '#/$defs/name'
+        },
+        null,
+        {
+          schemas: {},
+          parent: {
+            type: JSTypeName.OBJECT,
+            $defs: {
+              'name': {
+                type: JSTypeName.STRING
+              }
+            }
+          }
+        }
+      )
+      expect(schema).toEqual({ type: 'string' })
+    })
+    test("should return the request if it's already a schema", () => {
+      const source = { type: JSTypeName.STRING }
+      const schema = resolver.getRequestSchema(source)
+      expect(schema).toBe(source)
+    })
+  })
+})
